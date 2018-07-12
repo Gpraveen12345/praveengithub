1aae2278951adc89d90a7d0e84809e563e886475
praveen G <37699745+Gpraveen12345@users.noreply.github.com>
2018-07-12 13:03:29 +0530

    Update third.py

--- third.py
+++ third.py
@@ -3,6 +3,6 @@ n=n.lower()
 if(n=='a' or n=='e' or n=='i' or n=='o' or n=='u'):
 	print ("Vowel")
 elif(ord(n)>=97 and ord(n)<=123):
-	print("Constent")
+	print("Consonant")
 else:
 	print("invalid")
