51887cb93d210bcad1701468cdbb6368c9fb2471
praveen G <37699745+Gpraveen12345@users.noreply.github.com>
2018-07-12 16:11:12 +0530

    Create 16.py

---
+++ 16.py
@@ -0,0 +1,14 @@
+a=input()
+a=a.split()
+a[0]=int(a[0])
+a[1]=int(a[1])
+for i in range(a[0]+1,a[1]):
+	for j in range(2,i):
+		if(i%j==0):
+			break
+	else:
+		if(a[1]-1<>i):
+			print(i,end=' ')
+		else:
+			print(i)
+		
