dd08780aa83c6534908b71a0c5f8618d9cb9c41e
praveen G <37699745+Gpraveen12345@users.noreply.github.com>
2018-07-12 15:59:56 +0530

    Update 15.py

--- 15.py
+++ 15.py
@@ -1,7 +1,11 @@
+
 a=input()
 a=a.split()
 a[0]=int(a[0])
 a[1]=int(a[1])
 for i in range(a[0]+1,a[1]):
 	if(i%2==0):
-		print(i,end=' ')
+		if(a[1]-1==i):
+			print(i)
+		else:
+			print(i,end=" ")
